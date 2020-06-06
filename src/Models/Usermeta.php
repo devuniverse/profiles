1cde6a3354073b2012b590f662ae4941b71d5cdb
devuniverse <ishimwe@gmail.com>
2020-06-06 04:08:21 -0400

    Prelaunch update

--- src/Models/Usermeta.php
+++ src/Models/Usermeta.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Usermeta extends Model
 {
     protected $table='usermeta';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id','meta_key', 'meta_value'
+    ];
 }
